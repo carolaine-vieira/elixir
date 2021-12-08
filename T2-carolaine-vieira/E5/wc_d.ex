e41cc13769e6ca61e46a90e4fe74f29be8140cf5
Carolaine Vieira <54178722+carolaine-vieira@users.noreply.github.com>
2021-12-08 09:59:41 -0300

    E5 word count doc

--- T2-carolaine-vieira/E5/wc_d.ex
+++ T2-carolaine-vieira/E5/wc_d.ex
@@ -1,3 +1,24 @@
-arquivo = IO.gets("File to count the words from: ") |> String.trim()
-palavras = File.read!(arquivo) |> String.split(~r{(\\n|[^\w'])+}) |> Enum.filter(fn x -> x != "" end)
-palavras |> Enum.count |> IO.puts()
+defmodule WordCount do
+  @moduledoc """
+  Um modulo para contar palavras.
+  """
+  
+  @doc """
+  A funcao em si para contar palavras.
+
+  ## Parametros
+
+    - Para invocar a funcao, nenhum.
+
+  ## Exemplo
+
+      iex> WordCount.count
+
+  """
+  @spec count() :: integer
+  def count do
+    arquivo = IO.gets("File to count the words from: ") |> String.trim()
+    palavras = File.read!(arquivo) |> String.split(~r{(\\n|[^\w'])+}) |> Enum.filter(fn x -> x != "" end)
+    palavras |> Enum.count |> IO.puts()
+  end
+end
