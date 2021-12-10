e2e66799b7ac575f36b62e0301f8747544f55001
Carolaine Vieira <54178722+carolaine-vieira@users.noreply.github.com>
2021-12-09 22:01:21 -0300

    Todo doc finalizado

--- T2-carolaine-vieira/E5/todo_d.ex
+++ T2-carolaine-vieira/E5/todo_d.ex
@@ -1,9 +1,39 @@
 defmodule Todo do
+  @moduledoc """
+  Digamos que um gerenciador de tarefas.
+  """
+
+  @doc """
+  Funcao inicial.
+
+  ## Parametros
+
+    - Para invocar a funcao, nenhum.
+
+  ## Exemplo
+
+      iex> Todo.start()
+
+  """
+  @spec start() :: integer
   def start do
     filename = IO.gets("Name of .csv to load: ") |> String.trim
     read(filename)
   end
 
+  @doc """
+  Funcao com menu para interacao.
+
+  ## Parametros
+
+    - String: arquivo a ser lido.
+
+  ## Exemplo
+
+      iex> Todo.get_command(r)
+
+  """
+  @spec get_command(String)
   def get_command(data) do
     prompt = """
     Type the first letter of the commando you want to run \n
@@ -15,12 +45,24 @@ defmodule Todo do
 
     case command do
       "r"        -> show_todos(data)
-      #"d"        -> delete_todos(data)
+      "d"        -> delete_todos(data)
       "q"        -> "Goobye"
       _          -> get_command(data)
     end
   end
 
+  @doc """
+  Funcao para deletar todos.
+
+  ## Parametros
+
+    - Nenhum para chamar a funcao.
+
+  ## Exemplo
+
+      iex> Todo.delete_todos()
+
+  """
   def delete_todos() do
     todo = IO.gets("Which todo would you like to delete? \n") |> String.trim
     if Map.has_key? data, todo do
@@ -35,6 +77,18 @@ defmodule Todo do
     end
   end
 
+  @doc """
+  Funcao para deletar todos.
+
+  ## Parametros
+
+    - String: nome do arquivo que deve ser lido.
+
+  ## Exemplo
+
+      iex> Todo.read(nome_do_arquivo)
+
+  """
   def read(filename) do
     case File.read(filename) do 
       {:ok, body}      -> body
@@ -44,12 +98,37 @@ defmodule Todo do
     end
   end
 
+  @doc """
+  Funcao para tratamento do corpo.
+
+  ## Parametros
+
+    - String
+
+  ## Exemplo
+
+      iex> Todo.parse(body)
+
+  """
   def parse(body) do
     [header | lines] = String.split(body, ~r{(\r\n|\r|\n)})
     titles = tl String.split(header, ",")
     parse_lines(lines, titles)
   end
 
+  @doc """
+  Funcao para tratamento das linhas.
+
+  ## Parametros
+
+    - String
+    - String
+
+  ## Exemplo
+
+      iex> Todo.parse_lines(lines, titles)
+
+  """
   def parse_lines(lines, titles) do
     Enum.reduc(lines, %{}, fn line, built ->
       [name | fields] = String.split(line, ",")
@@ -62,6 +141,19 @@ defmodule Todo do
     end)
   end
 
+  @doc """
+  Funcao para mostrar tarefas.
+
+  ## Parametros
+
+    - String
+    - String
+
+  ## Exemplo
+
+      iex> Todo.parse_lines(lines, titles)
+
+  """
   def show_todos(data, next_command? \\ true) do
     itens = Map.keys data
     IO.puts "You have the following Todos: \n"
@@ -71,6 +163,4 @@ defmodule Todo do
       get_command(data)
     end
   end
-
-  #20min - https://alchemist.camp/episodes/minimal-todo-1
 end
